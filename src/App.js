5f29614f24dbe3ac48b27464375d1b41558059b0
Hmr <hmr@pop-os.localdomain>
2020-11-06 14:03:23 -0400

    Changes in the calculator

--- src/App.js
+++ src/App.js
@@ -12,9 +12,15 @@ function App() {
   
 
   const [dolarprice, setDolarprice] = useState("");
+  const [dolar, setDolar] = useState("");
+  const [bolivar, setBolivar] = useState("");
   var date = moment()
 
   var hoy= date.locale('es').format('LLL')
+  
+   
+
+
   
 
   useEffect(()=>{
@@ -33,7 +39,31 @@ function App() {
   
   }, []);
   
+  function handleChange(e){
 
+    setDolar(e.target.value)
+    
+    const result = dolarprice * dolar
+
+   console.log(result)
+    
+  setBolivar(result)
+
+    }
+
+
+    function handleChange1(e){
+
+      setBolivar(e.target.value)
+      
+      const result = bolivar / dolarprice
+  
+     console.log(result)
+      
+    setDolar(result)
+  
+      }
+  
 
   
 
@@ -43,11 +73,21 @@ function App() {
 <div class="flex h-screen">
   <div class="m-auto text-center">
   Dolar del {hoy}
-  <div className="font-hairline text-6xl text-green-700"> $1 <span className="text-black">=</span> </div>
+  
   <div className="font-hairline text-6xl"><span className="t text-teal-700">{dolarprice} </span> Bolivares</div> 
   </div>
+
+
 </div>
-  
+<div className="flex flex-row">
+      <span className="flex items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold text-grey-darker" >$</span>
+      <input type="number"  value={dolar} name="price" className="bg-grey-lighter text-grey-darker py-2  rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold"onChange={handleChange}></input>
+  </div>
+
+   <div className="flex flex-row">
+   <span className="flex items-center bg-grey-lighter rounded rounded-r-none px-3 font-bold text-grey-darker">Bs</span>
+   <input type="number" value={bolivar} name="price" className="bg-grey-lighter text-grey-darker py-2  rounded text-grey-darkest border border-grey-lighter rounded-l-none font-bold" onChange={handleChange1}></input>
+</div>
     
     </>
   );
